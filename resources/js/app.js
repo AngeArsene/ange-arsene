db544ae3795b81a68d139c8ffad401e068e89f57
AngeArsene <nkenmandenga@gmail.com>
2026-09-21 11:27:57 +0100

    feat: update CSS styles for improved theming and layout and toggle functionality

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
-//
+document.querySelectorAll('[data-theme-toggle]').forEach((button) => {
+	button.addEventListener('click', () => {
+		document.documentElement.classList.toggle('light-theme');
+		button.setAttribute('aria-pressed', document.documentElement.classList.contains('light-theme').toString());
+	});
+});
